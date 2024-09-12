28376a1931b27e677dc08b326e189a27321ba03e
seblful <alesha.chimba@gmail.com>
2024-09-12 11:38:37 +0300

    [EDIT] read json

--- formatter.py
+++ formatter.py
@@ -1,3 +1,22 @@
+import os
+
+import json
+import csv
+
+
 class RewordFormatter:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 data_dir: str) -> None:
+        # Paths
+        self.inputs_dir = os.path.join(data_dir, "inputs")
+        self.outputs_dir = os.path.join(data_dir, "outputs")
+
+    @staticmethod
+    def open_json(json_path: str) -> dict:
+        with open(json_path, "r", encoding="utf-8") as file:
+            json_dict = json.load(file)
+
+        return json_dict
+
+    def format_json(self, json_name: str) -> None:
+        json_path = os.path.join(self.inputs_dir(json_name))
