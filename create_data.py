f00d8702c402431141080ef251549d043466e0c9
seblful <alesha.chimba@gmail.com>
2024-09-12 11:27:01 +0300

    [ADD] init commit

---
+++ create_data.py
@@ -0,0 +1,14 @@
+import os
+
+HOME = os.getcwd()
+DATA = os.path.join(HOME, "data")
+
+JSON_NAME = os.path.join(INPUTS, "phrasal_verbs.json")
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
