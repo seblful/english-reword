783e38c19cc7e94e9af7b2b20113968a2dbd03f9
seblful <alesha.chimba@gmail.com>
2024-09-12 12:01:31 +0300

    [EDIT] script to create data

--- create_data.py
+++ create_data.py
@@ -1,13 +1,18 @@
 import os
 
+from formatter import RewordFormatter
+
 HOME = os.getcwd()
 DATA = os.path.join(HOME, "data")
 
-JSON_NAME = os.path.join(INPUTS, "phrasal_verbs.json")
+JSON_NAME = "phrasal_verbs.json"
 
 
 def main() -> None:
-    pass
+    formatter = RewordFormatter(data_dir=DATA,
+                                max_words=200)
+
+    formatter.format_json(json_name=JSON_NAME)
 
 
 if __name__ == "__main__":
